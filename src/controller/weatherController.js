345e4f3807ff38816adb70238cd3c64027e91e03
Waynexlink <wm500972@gmail.com>
2025-03-25 20:16:56 +0100

    minor-fixes

--- src/controller/weatherController.js
+++ src/controller/weatherController.js
@@ -4,15 +4,15 @@ exports.getWeatherdata = async (req, res) => {
   const city = req.params.city;
   const cacheData = await getWeatherCache(city);
   if (cacheData) {
-    return res.json({ source: Cache, data: cacheData });
+    return res.json({ source: "Cache", data: cacheData });
   }
   try {
     const url = `${process.env.WEATHER_API_URL}/${city}?key=${process.env.WEATHER_API_KEY}`;
 
     const response = await axios.get(url);
-    res.send(response.data);
+
     await setWeatherCache(city, response.data, 5000);
-    return res.json({ source: API, data: response.data });
+    return res.json({ source: "API", data: response.data });
   } catch (error) {
     res.status(500).json({
       error: "failed to fetch weather data ",
